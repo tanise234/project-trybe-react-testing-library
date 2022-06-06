b5436ff52c0b18b8ebe057523a9565147ed4492f
Tanise Santos <tanise234@gmail.com>
2022-06-06 17:32:15 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,2 +1,16 @@
-test('', () => {});
-// tanise
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  renderWithRouter(<App />);
+
+  const linkHome = screen.getByRole('link', { name: /Home/i });
+  const linkAbout = screen.getByRole('link', { name: /About/i });
+  const linkFavorites = screen.getByRole('link', { name: /Favorite Pokémons/i });
+
+  expect(linkHome).toBeInTheDocument();
+  expect(linkAbout).toBeInTheDocument();
+  expect(linkFavorites).toBeInTheDocument();
+});
